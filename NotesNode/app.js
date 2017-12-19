c3c4063ecd7a273717ca67c99a0ffd0e8cd78e3b
Clyve Widjaya <Clyve@Clyves-MacBook-Pro.local>
2017-12-19 01:38:08 -0500

    Update again

--- NotesNode/app.js
+++ NotesNode/app.js
@@ -1,51 +1,22 @@
 console.log('Starting app.js');
 
-const fs = require('fs'); //const is like val in scala. we normally use var, const n var, once they are declared
-                          //they cannot be changed.
-                          //require param is only 1. this case we put fs
-
-const os = require('os'); //this case we need os.
-const notes = require('./notes.js');
+const fs = require('fs');
 const _ = require('lodash');
 
-//require is used to load in modules example from nodejs.org/api or other files or import packages from like
-//npmjs.com
-//something like import in java.
+const notes = require('./notes.js');
 
-//fs.appendFile('greetings.txt','Hello World');
-//wont work anymore for version 7 and up
+//console.log(process.argv);
+var command = process.argv[2];
+console.log('Command: ' + command);
 
-var user = os.userInfo();
-//console.log(user);
-
-//option 1
-/*
-fs.appendFile('greetings.txt','Hello ' + user.username + '!\n' + user.homedir, function(err){
-  if (err) {
-    console.log('Unable to write to file');
-  }
-}); //this will catch error and print them to the terminal.
-*/
-//option 2
-//fs.appendFileSync('greetings.txt','Hello Clyve'); //better to use option 1
-
-//template string use ``
-/*
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} years old.`, function(err){
-  if (err) {
-    console.log('Unable to write to file');
-  }
-}); //similar like scala.
-*/
-
-var res = notes.addNote();
-console.log(res);
-
-var sum = notes.add(1,2);
-console.log(sum);
-
-//use some utilites example of lodash
-console.log(_.isString('123')); //true
-
-var filteredArray = _.uniq(['Clyve',1,'Clyve',1,2,3,4]);
-console.log(filteredArray);
+if (command === 'add'){
+  console.log('Adding new note');
+} else if (command === 'list'){
+  console.log('Listing all notes');
+} else if (command === 'read'){
+  console.log('Fetching note => ' + process.argv[3])
+} else if (command === 'remove'){
+  console.log('Removing note => '+ process.argv[3])
+} else {
+  console.log('Command not recognized');
+}
--- NotesNode/notes.js
+++ NotesNode/notes.js
@@ -1,16 +1,8 @@
 console.log('Starting notes.js');
 
 //console.log(module);
-
-//what we need the most is exports.
-module.exports.age = 25;
-
 //we can ofc store functions
 module.exports.addNote = () => {
   console.log('addNote');
   return 'New Note';
-};
-
-module.exports.add = (a,b) => {
-  return a + b;
 };
