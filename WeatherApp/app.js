af62db34fd6b2eee3b4b74b91409e18ffa66256f
Clyve Widjaya <Clyve@Clyves-MacBook-Pro.local>
2017-12-25 01:47:33 -0500

    added error handling on app.js

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -24,8 +24,14 @@ request({
   //console.log(JSON.stringify(body, undefined, 2)); //this will do as pretty print for json. second argument will
                                                     //always be undefined for most. third is 2 or 4
   //console.log(JSON.stringify(error, undefined, 2));
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-  console.log(`Place ID: ${body.results[0].geometry.location_type}`);
+  if (error){
+    console.log('Unable to connect to Google servers');
+  } else if (body.status === 'ZERO_RESULTS'){
+    console.log('Unable to find that address');
+  } else if (body.status === 'OK'){
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    console.log(`Place ID: ${body.results[0].geometry.location_type}`);
+  }
 });
