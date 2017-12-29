9d9dbca50904e387696df2217b8b3d3f2c19f79a
Clyve Widjaya <Clyve@Clyves-MacBook-Pro.local>
2017-12-29 01:57:05 -0500

    very tini change

--- WebServer/server.js
+++ WebServer/server.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
   next(); //we need next in order to continue the program.
 });
 
+//next is quite useful when you have like a maintenance thing.
 /*
 app.use((req,res,next) => {
   res.render('maintenance.hbs');
