922dc73c1c0fb9041584905d3bb3783a8db3c5c3
Clyve Widjaya <Clyve@Clyves-MacBook-Pro.local>
2017-12-29 01:55:58 -0500

    Half done with website server lesson

--- WebServer/server.js
+++ WebServer/server.js
@@ -1,9 +1,66 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err){
+      console.log('Unable to append to server.log')
+    }
+  });
+  next(); //we need next in order to continue the program.
+});
+
+/*
+app.use((req,res,next) => {
+  res.render('maintenance.hbs');
+});
+*/
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+//localhost:3000/
+app.get('/',(req,res) => {
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Hello Clyve'
+  });
+});
+
+//localhost:3000/about
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page'
+  });
+});
+
+//localhost:3000/bad
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000);
+
 //get needs 2 param, url, and function to run, what to sent back to the request
 //localhost:3000/
+/*
 app.get('/',(req, res) => {
   //res.send('<h1>hello Clyve</h1>');
   res.send({
@@ -14,16 +71,4 @@ app.get('/',(req, res) => {
     age: '25'
   });
 });
-
-//localhost:3000/about
-app.get('/about',(req,res) => {
-  res.send('About Page');
-});
-
-//localhost:3000/bad
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable to handle request'
-  });
-});
-app.listen(3000);
+*/
